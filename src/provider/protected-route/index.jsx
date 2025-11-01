07a5990b97689c19430fa795b18da32bbd3206a1
Rizwan <152837388+M-Rizwan86@users.noreply.github.com>
2025-11-01 16:40:17 +0500

    Update index.jsx

--- src/provider/protected-route/index.jsx
+++ src/provider/protected-route/index.jsx
@@ -5,8 +5,13 @@ import { Navigate } from 'react-router-dom'
 export default function ProtectedRoute({ children }) {
     const { firebaseUser , loading } = useContext(AuthContext)
     const { uid } = firebaseUser || {}
+     const navigate = useNavigate()
     console.log(loading,uid)
-    
+        useEffect(() =>{
+        if (!loading && !uid) {
+            return navigate('/login')
+        }
+    }, [uid ,loading , navigate])
     if(loading && uid === undefined){
         console.log("running")
        return <Navigate to='/login' replace />
@@ -18,4 +23,5 @@ export default function ProtectedRoute({ children }) {
     return (
         uid ? <>{children}</> : <Navigate to='/login' replace />
     )
+
 }
